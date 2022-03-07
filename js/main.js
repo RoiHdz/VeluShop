6bf954200e054c906616e511f57cfa4674dc06ea
Rodrigo Hernandez <rodrigophernandez@hotmail.com>
2022-03-07 01:29:45 -0600

    2/4 Del sitio publico

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Funciones para cambiar de favoritos
+function addFavs(icon,iconFill) {
+    icon.classList.add("inactive");
+    iconFill.classList.remove("inactive");
+}
+function removeFavs(iconFill,icon) {
+    iconFill.classList.add("inactive")
+    icon.classList.remove("inactive")
+}
+
+
+
+
