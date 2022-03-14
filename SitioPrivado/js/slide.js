9d103d2ad4fc1c187f671890565cad33d22ef461
Rodrigo Hernandez <rodrigophernandez@hotmail.com>
2022-03-13 21:44:32 -0600

    Actualizacion de menu sitio privado

--- SitioPrivado/js/slide.js
+++ SitioPrivado/js/slide.js
@@ -1,10 +1,19 @@
-let btn = document.querySelector("#btn");
-let sidebar = document.querySelector(".sidebar");
-let searchBtn = document.querySelector(".bx-search");
 
-btn.onclick = function(){
-    sidebar.classList.toggle("active");
+//Togle menu
+let toggle = document.querySelector('.toggle');
+let navegacion = document.querySelector('.navegacion');
+let main_topbar = document.querySelector('.main_topbar');
+
+toggle.onclick = function(){
+    navegacion.classList.toggle('active');
+    main_topbar.classList.toggle('active');
 }
-searchBtn.onclick = function(){
-    sidebar.classList.toggle("active");
+
+//Agregar clase hovered cuando se seleccione un item
+let list = document.querySelectorAll('.navegacion li');
+function activeLinks() {
+    list.forEach((item) =>
+        item.classList.remove('hovered'));
+        this.classList.add('hovered');
 }
+list.forEach((item) => item.addEventListener('mouseover', activeLinks));
